54f7062f435d180fada472d3e853360784d08964
Sal Aldana <saldana@level3studios.net>
2018-01-11 14:31:22 -0700

    WebApp Update
    Added LoginModel used in Index Page. Updated Index page with layout from Sketch design.

---
+++ webapp/BumblebeeASP/Models/LoginModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace BumblebeeASP.Models
+{
+    public class LoginModel
+    {
+        [Required(ErrorMessage = "Email address is required")]
+        [Display(Prompt = "email address")]
+        public string emailAddress { get; set; }
+
+        [Required(ErrorMessage = "Password is required")]
+        [Display(Prompt = "password")]
+        public string password { get; set; }
+    }
+}
