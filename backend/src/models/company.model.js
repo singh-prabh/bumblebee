5ab2455d6e7716ff7fe68b134b0e0799bd047cc3
Sal Aldana <saldana@level3studios.net>
2018-01-10 22:07:13 -0700

    API update
    Added company model and services.

---
+++ backend/src/models/company.model.js
@@ -0,0 +1,29 @@
+// See http://docs.sequelizejs.com/en/latest/docs/models-definition/
+// for more of what you can do here.
+const Sequelize = require('sequelize');
+const DataTypes = Sequelize.DataTypes;
+
+module.exports = function (app) {
+  const sequelizeClient = app.get('sequelizeClient');
+  const company = sequelizeClient.define('company', {
+    name: {
+      type: DataTypes.STRING
+    }, 
+    website: {
+      type: DataTypes.STRING
+    }
+  }, {
+    hooks: {
+      beforeCount(options) {
+        options.raw = true;
+      }
+    }
+  });
+
+  company.associate = function (models) { // eslint-disable-line no-unused-vars
+    // Define associations here
+    // See http://docs.sequelizejs.com/en/latest/docs/associations/
+  };
+
+  return company;
+};
--- backend/src/services/index.js
+++ backend/src/services/index.js
@@ -1,6 +1,8 @@
 const users = require('./users/users.service.js');
 const person = require('./person/person.service.js');
+const company = require('./company/company.service.js');
 module.exports = function (app) {
   app.configure(users);
   app.configure(person);
+  app.configure(company);
 };
